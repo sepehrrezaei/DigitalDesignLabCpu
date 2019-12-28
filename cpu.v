da83893ca55d5fb79fee43f689cbbe4f8eb9d520
frdfgh <40581974+frdfgh@users.noreply.github.com>
2019-12-28 21:58:16 +0330

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -1,5 +1,5 @@
 module cpu();
-wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data,instruction_part,ALUSrc, MemtoReg, RegWrite,Branch, ALUOp1, ALUOp0,operation_code;
+wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data,instruction_part,ALUSrc, MemtoReg, RegWrite,Branch, ALUOp1, ALUOp0,operation_code,in1, in2, control;
 wire [31:0] Databus1, Databus2, Databus3;
 wire [63:0] Instruction;
 integer write=1,reset=0;
@@ -20,6 +20,7 @@ memory memory1( .clk(clk), .Address(Address), .Write_data(Write_data), .Read_dat
 	
 control control1(.instruction_part(instruction_part),.ALUSrc(ALUSrc), .MemtoReg(MemtoReg), .RegWrite(RegWrite),.MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOp1(ALUOp1), .ALUOp0(ALUOp0));
 
+ALU ALU1(.in1(in1), .in2(in2), .control(control));
 aluCon aluCon1(.ALUOp1(ALUOp1), .ALUOp0(ALUOp0), .instruction_part(instruction_part), .operation_code(operation_code));
 
 	
