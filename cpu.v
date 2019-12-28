24ea09e4012f50c984a9cb3514dc6c102df7bbdc
frdfgh <40581974+frdfgh@users.noreply.github.com>
2019-12-28 21:44:16 +0330

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -1,5 +1,5 @@
 module cpu();
-wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite;
+wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data;
 wire [31:0] Databus1, Databus2, Databus3;
 wire [63:0] Instruction;
 integer write=1,reset=0;
@@ -11,7 +11,10 @@ pc pc1(.clk(clk), .data_out(data_out) , .pcout(pcout) , .reset(reset) ,.write(wr
 Reg register_file1(.clk(clk), .RegWrite(RegWrite), 
 		.Read_register1(Instruction[19:15]), .Read_register2(Instruction[24:20]), .Write_register(Instruction[11:7]),
 		.Write_data(Databus3), .Read_data1(Databus1), .Read_data2(Databus2));
+
 	
+multiplexer (.input_data_1(input_data_1), .input_data_2(input_data_2), .input_select(input_select), .output_data(output_data));
+
 memory memory1( .clk(clk), .Address(Address), .Write_data(Write_data), .Read_data(Read_data), .MemRead(MemRead), .MemWrite(MemWrite))
 
 endmodule;
