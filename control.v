b7afd26de3cfc0f37f9cc4d4c0b3ab477a202145
Sepehr Rezaei <sepehrrezaei99@gmail.com>
2019-12-28 15:46:31 +0330

    controller and multiplexer added

---
+++ control.v
@@ -0,0 +1,26 @@
+module control (
+    instruction_part,
+    ALUSrc, MemtoReg, RegWrite,
+    MemRead, MemWrite, Branch, ALUOp1, ALUOp0
+);
+
+    input [6:0] instruction_part;
+    output ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp0;
+    reg [7:0] value;
+
+    assign { ALUSrc, MemtoReg, RegWrite, MemRead, MemWrite, Branch, ALUOp1, ALUOp0} = value;
+     
+
+    always @ (instruction_part) begin
+    
+        casex(instruction_part)
+            7'b0110011: value = 8'b00100010;
+            7'b0000011: value = 8'b11110000;
+            7'b0100011: value = 8'b1x001000;
+            7'b1100011: value = 8'b0x000101;
+            7'b0: value = 8'b0;
+        endcase
+
+    end
+
+endmodule
---
+++ multiplexer.v
@@ -0,0 +1,9 @@
+module multiplexer (input_data_1, input_data_2, input_select, output_data);
+    parameter n = 64;
+	input input_select;
+	input [n - 1 : 0] input_data_1, input_data_2;
+	output[n - 1 : 0] output_data;
+
+	assign output_data = input_select ? input_data_2 : input_data_1;
+
+endmodule
