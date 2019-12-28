e649eabde794c6d8cdf109b68a9acb432fb36033
frdfgh <40581974+frdfgh@users.noreply.github.com>
2019-12-28 21:48:41 +0330

    Update cpu.v

--- cpu.v
+++ cpu.v
@@ -1,5 +1,5 @@
 module cpu();
-wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data;
+wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data,instruction_part,ALUSrc, MemtoReg, RegWrite,Branch, ALUOp1, ALUOp0;
 wire [31:0] Databus1, Databus2, Databus3;
 wire [63:0] Instruction;
 integer write=1,reset=0;
@@ -16,5 +16,8 @@ Reg register_file1(.clk(clk), .RegWrite(RegWrite),
 multiplexer multiplexer1(.input_data_1(input_data_1), .input_data_2(input_data_2), .input_select(input_select), .output_data(output_data));
 
 memory memory1( .clk(clk), .Address(Address), .Write_data(Write_data), .Read_data(Read_data), .MemRead(MemRead), .MemWrite(MemWrite))
+
+	
+control (.instruction_part(instruction_part),.ALUSrc(ALUSrc), .MemtoReg(MemtoReg), .RegWrite(RegWrite),.MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOp1(ALUOp1), .ALUOp0(ALUOp0));
 
 endmodule;
