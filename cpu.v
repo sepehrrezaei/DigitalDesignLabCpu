a2d01b95e571d82f95c0ee2cd5d1130fece081a8
Sepehr Rezaei <sepehrrezaei99@gmail.com>
2019-12-29 09:10:28 +0330

    imm gen added to cpu

--- cpu.v
+++ cpu.v
@@ -1,6 +1,7 @@
 module cpu();
-wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,output_data,instruction_part,ALUSrc, MemtoReg, RegWrite,Branch, ALUOp1, ALUOp0,operation_code,in1, in2, control,aluOut;
-wire [63:0] Databus1, Databus2, DataWriteRegister;
+wire clk ,RD_Address,Instruction,pcout,Address,Write_data,Read_data,MemRead,MemWrite,input_data_1,input_data_2,input_select,instruction_part,ALUSrc, MemtoReg, RegWrite,Branch, ALUOp1, ALUOp0,operation_code,in1, in2, control,aluOut,outImmGen;
+wire [63:0] Databus1, Databus2, DataWriteRegister,multi1Out;
+
 wire [63:0] Instruction;
 integer write=1,reset=0;
 clock clock1(.clk(clk));
@@ -15,11 +16,13 @@ Reg register_file1(.clk(clk), .RegWrite(RegWrite),
 
 control control1(.instruction_part(Instruction[6:0]),.ALUSrc(ALUSrc), .MemtoReg(MemtoReg), .RegWrite(RegWrite),.MemRead(MemRead), .MemWrite(MemWrite), .Branch(Branch), .ALUOp1(ALUOp1), .ALUOp0(ALUOp0));
 
-multiplexer multiplexer1(.input_data_1(input_data_1), .input_data_2(input_data_2), .input_select(input_select), .output_data(output_data));
+ImmGen immGen1(.Instruction(Instruction),.output_data(outImmGen));
+
+multiplexer multiplexer1(.input_data_1(Databus2), .input_data_2(outImmGen), .input_select(ALUSrc), .output_data(multi1Out));
 
 memory memory1( .clk(clk), .Address(aluOut), .Write_data(Databus2), .Read_data(Read_data), .MemRead(MemRead), .MemWrite(MemWrite));
 
-alu ALU1(.in1(Databus1), .in2(Databus2), .control(operation_code),.out(aluOut));
+alu ALU1(.in1(Databus1), .in2(multi1Out), .control(operation_code),.out(aluOut));
 
 aluCon aluCon1(.ALUOp1(ALUOp1), .ALUOp0(ALUOp0), .instruction_part({Instruction[31:25],Instruction[14:12]}}), .operation_code(operation_code));
 
